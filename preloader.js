3f0046b63707d36c169eb3c20ab63a2e528da8eb
Imtiaz Ahammed Nafiz <93529122+TechLead-G@users.noreply.github.com>
2022-02-17 08:06:10 -0800

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -6,8 +6,3 @@ function myFunction() {
     preloader.style.display = 'none';
 
 }
-
-alert("Our Boss Love and Like Yusra Mohamed");
-alert("Our Boss Love and Like Yusra Mohamed");
-alert("Our Boss Love and Like Yusra Mohamed");
-alert("But, She is so Angry to hear this");
